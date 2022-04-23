6fc706fc23c32a70e9a2f19147bc67b64aff5aee
marcosebsilva <73955653+marcosebsilva@users.noreply.github.com>
2022-04-23 20:43:24 -0300

    style: :lipstick: global styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
+import React from 'react';
 import {
   Routes,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Settings from './pages/Settings';
 import Question from './pages/Question';
@@ -10,21 +11,20 @@ import Contacts from './components/Contacts';
 import Footer from './components/Footer';
 import LinkToArticle from './components/LinkToArticle';
 
-
 function App() {
   return (
     <>
       <Contacts />
       <LinkToArticle />
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
         <Route path="/leaderboard" element={<Settings />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/questions/:id" element={<Question />} />
+        <Route path="/question/:id" element={<Question />} />
       </Routes>
       <Footer>Made by Marcos Silva</Footer>
     </>
-  )
+  );
 }
 
 export default App;
